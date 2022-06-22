43859142090500660b98eea1921a7560e8b45a6c
felixcordova <fcordova@multileaf.io>
2022-06-22 01:59:21 -0500

    se modifico tamanos

---
+++ node.js
@@ -0,0 +1,9 @@
+document.getElementById('btn1').addEventListener('click',function(){
+    body = document.getElementById('global');
+    body.style.background = 'rgba(59, 130, 246, 0.5)';
+    }) 
+    
+    document.getElementById('btn2').addEventListener('click',function(){
+      body = document.getElementById('global');
+      body.style.background =  'darkblue';
+    })
